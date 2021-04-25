bf3e4168145b4e0d33f7d015853c1ae8c6a56790
sanoussibarkay <55973917+sanoussibarkay@users.noreply.github.com>
2021-04-26 03:54:30 +0800

    Update deletevehicle.php

--- deletevehicle.php
+++ deletevehicle.php
@@ -2,7 +2,7 @@
 
    $id= $_GET['id'];
 
-   $conn=mysqli_connect('localhost','root','','vehicle management');
+   $conn=mysqli_connect("remotemysql.com","mxGxQeDMmY","29L092KTIJ","mxGxQeDMmY");
 
    $sql="DELETE FROM `booking` WHERE booking_id='$id'";
     echo $sql;
