025953bd8862068627410b3f94338f0480311fca
sanoussibarkay <55973917+sanoussibarkay@users.noreply.github.com>
2021-04-26 03:53:23 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn=mysqli_connect("remotemysql.com","oLK33sA5lz","pVYs7yUo1W","oLK33sA5lz");
+$conn=mysqli_connect("remotemysql.com","mxGxQeDMmY","29L092KTIJ","mxGxQeDMmY");
 if (!$conn) {
   die("connection failed:".mysqli_connect_error());
 }
