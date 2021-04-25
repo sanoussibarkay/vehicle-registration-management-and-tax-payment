96611f33fb1fef2b259f71137c7ff74d30c248af
sanoussibarkay <55973917+sanoussibarkay@users.noreply.github.com>
2021-04-26 03:07:34 +0800

    Update deletecomplaint.php

--- deletecomplaint.php
+++ deletecomplaint.php
@@ -2,7 +2,7 @@
 
    $id= $_GET['id'];
 
-   $conn=mysqli_connect('localhost','root','','vehicle management');
+   $conn=mysqli_connect("remotemysql.com","oLK33sA5lz","pVYs7yUo1W","oLK33sA5lz");
 
    $sql="DELETE FROM `comments` WHERE comment_id='$id'";
     echo $sql;
