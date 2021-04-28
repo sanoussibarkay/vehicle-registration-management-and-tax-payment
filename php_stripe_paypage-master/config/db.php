a9e2e6c01abb7fe1f8a5e06a21c9de34ab47bab9
sanoussibarkay <55973917+sanoussibarkay@users.noreply.github.com>
2021-04-29 04:55:37 +0800

    Update db.php

--- php_stripe_paypage-master/config/db.php
+++ php_stripe_paypage-master/config/db.php
@@ -1,6 +1,6 @@
 <?php
   // DB Params
-  define("DB_HOST", "localhost");
-  define("DB_USER", "root");
-  define("DB_PASS", "");
-  define("DB_NAME", "paypage");
+  define("DB_HOST", "remotemysql.com");
+  define("DB_USER", "mxGxQeDMmY");
+  define("DB_PASS", "29L092KTIJ");
+  define("DB_NAME", "mxGxQeDMmY");
