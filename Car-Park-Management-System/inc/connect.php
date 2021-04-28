40c705be5f9a7da7e1664cc6c8ac882fc8b9a4d7
sanoussibarkay <55973917+sanoussibarkay@users.noreply.github.com>
2021-04-29 04:54:04 +0800

    Update connect.php

--- Car-Park-Management-System/inc/connect.php
+++ Car-Park-Management-System/inc/connect.php
@@ -1,20 +1,20 @@
 <?php
 
 
-$db_host		= 'localhost';
-$db_user		= 'root';
-$db_pass		= '';
-$db_database	= 'cpms'; 
+$db_host		= 'remotemysql.com';
+$db_user		= 'mxGxQeDMmY';
+$db_pass		= '29L092KTIJ';
+$db_database	= 'mxGxQeDMmY';
 
 
 $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "cpms";
+$servername = "remotemysql.com";
+$username = "mxGxQeDMmY";
+$password = "29L092KTIJ";
+$dbname = "mxGxQeDMmY";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -23,5 +23,5 @@ if ($conn->connect_error) {
 
 
 
-$connect=mysqli_connect("localhost", "root", "", "cpms") or die(mysqli_error());
+$connect=mysqli_connect("remotemysql.com","mxGxQeDMmY","29L092KTIJ","mxGxQeDMmY") or die(mysqli_error());
 ?>
